17c6ffec4bb0407e18601f6d3b7c255a30189952
Fabrice Flore-Thébault <themroc@centsix.org>
2012-02-28 16:34:02 +0100

    fix images resolution problem on pdf

--- images-resolution.sh
+++ images-resolution.sh
@@ -5,10 +5,7 @@ do
 	dpi=$(identify -units PixelsPerInch -verbose $pic | grep Resolution | cut -d x -f 2)
 	if [ 150 != $dpi -a "149.99" != $dpi ] 
 	then
-		echo "convert $pic to 150dpi"
-		convert -units PixelsPerInch -density 150 $pic $pic	
-	else
-		echo "Image density already set for $pic"
+		convert -units PixelsPerInch -density 150 $pic $pic
 	fi
 done
 
