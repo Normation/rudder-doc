5c0fc88f18ce6be761063ec85cbe53ae39a0a1f2
Alexis Mousset <alexis.mousset@normation.com>
2016-07-20 18:23:27 +0200

    Fixes #8727: Display link errors in doc build

--- tests/check_broken_links.sh
+++ tests/check_broken_links.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # A recursion level of 2 is enough because all pages are referenced in the left tree.
-linkchecker -q -r 2 --check-extern --ignore-url=^http://localhost --ignore-url=^https://you-rudder webhelp/index.html
+linkchecker -r 2 --check-extern --ignore-url=^http://localhost --ignore-url=^https://you-rudder webhelp/index.html
