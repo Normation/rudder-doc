960d4174dbf426e65138cab9925aab9fef143dfc
Alexis Mousset <alexis.mousset@normation.com>
2016-07-20 18:32:50 +0200

    Fixes #8726: Broken link in the doc

--- tests/check_broken_links.sh
+++ tests/check_broken_links.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # A recursion level of 2 is enough because all pages are referenced in the left tree.
-linkchecker -q -r 2 --check-extern --ignore-url=^http://localhost --ignore-url=^https://you-rudder webhelp/index.html
+# Add an exception for build.opensuse.org as they appear to have retrictions based on User-Agent
+linkchecker -q -r 2 --check-extern --ignore-url=^https://build.opensuse.org --ignore-url=^http://localhost --ignore-url=^https://you-rudder webhelp/index.html
