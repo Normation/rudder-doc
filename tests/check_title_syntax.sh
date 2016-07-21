ef949386c3942b868b1f392c953bd98a9e21f14e
Alexis Mousset <alexis.mousset@normation.com>
2016-07-21 15:48:10 +0200

    Fixes #8733: Add a test for maximum title level

--- tests/check_title_syntax.sh
+++ tests/check_title_syntax.sh
@@ -19,3 +19,24 @@ if [ ${ERROR} -ne 0 ]; then
 else
     echo "File ending by newlines: OK"
 fi
+
+# Test that there is not more than 5 "=" in title levels
+
+ERROR=0
+
+for entry in $(find $(dirname $0)/../*_* -name '*txt')
+do
+    LINE=$(grep -E "^======+ " ${entry})
+    if [ $? -eq 0 ]; then
+        echo "Incorrect title level in ${entry}: ${LINE}"
+        ERROR=1
+    fi
+done
+
+if [ ${ERROR} -ne 0 ]; then
+    echo "Error: This will likely break the manual formatting."
+    exit 1
+else
+    echo "Maximum title levelw: OK"
+fi
+
