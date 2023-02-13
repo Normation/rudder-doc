0334d8ff9e3c93fb95182de2fc22ad35e203c5de
Alexis Mousset <alexis.mousset@rudder.io>
2023-02-13 20:08:34 +0100

    Fixes #22368: Prepare 7.3 doc (#951)

--- src/reference/dependencies/tools/ncf_doc_rudder.py
+++ src/reference/dependencies/tools/ncf_doc_rudder.py
@@ -9,11 +9,7 @@ import ncf
 import requests
 import sys
 import re
-from distutils.version import StrictVersion
 from pprint import pprint
-
-# URL of the rudder_info api
-release_info_url = "http://www.rudder-project.org/release-info/rudder/"
 
 def slugify(s):
     s = "_" + s
@@ -22,47 +18,7 @@ def slugify(s):
     s = re.sub('\W', '_', s)
     return s
 
-def get_min_versions():
-  "Build the dictionary of minimal Rudder version including compatibility with a given CFEngine version"
-  min_version = {}
-  
-  try:
-    versions = requests.get(release_info_url + "versions/supported").content.decode('ascii').splitlines()
-  except requests.exceptions.RequestException as e:
-    print(e)
-    sys.exit(1)
-
-  for rudder_version in versions:
-    try:
-      cfengine_version = requests.get(release_info_url + "versions/" + rudder_version + "/components/cfengine").content.decode('ascii')
-    except requests.exceptions.RequestException as e:
-      print(e)
-      sys.exit(1)
-    
-    if cfengine_version in min_version:
-      if StrictVersion(rudder_version) >= StrictVersion(min_version[cfengine_version]):
-        continue
-    min_version[cfengine_version] = rudder_version
-    
-  return min_version
-
-def rudder_version(min_version, version):
-  """Extract minimal Rudder version"""
-  canonified_version = re.findall(r'>=? ?(\d+\.\d+)', version)[0]
-  
-  cfengine_versions = list(min_version.keys())
-  cfengine_versions.sort(key=StrictVersion)
-  
-  for cfengine_version in cfengine_versions:
-    if StrictVersion(canonified_version) <= StrictVersion(cfengine_version):
-      return min_version[cfengine_version]
-  return False
-
-
 if __name__ == '__main__':
-  # Get CFEngine <=> Rudder versions
-  versions = get_min_versions()
-
   # Get all generic methods
   generic_methods = ncf.get_all_generic_methods_metadata()["data"]["generic_methods"]
   
@@ -84,18 +40,12 @@ if __name__ == '__main__':
  
     # Generate markdown for each generic method
     for generic_method in categories[category]:
-      # do not display generic methods not compatible with a released Rudder version
-      rudder_version_needed = rudder_version(versions, generic_method["agent_version"])
-      if not rudder_version_needed:
-        continue
       bundle_name = generic_method["bundle_name"]
       content.append('\n### '+ bundle_name)
       content.append(generic_method["description"])
       if "deprecated" in generic_method:
         content.append('\n**WARNING**: This generic method is deprecated.')
         content.append(generic_method["deprecated"])
-
-      content.append('\nCompatible with nodes running Rudder '+rudder_version_needed+' or higher.')
       
       if "documentation" in generic_method:
         content.append('\n#### Usage')
